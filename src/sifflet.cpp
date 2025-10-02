8a641b32616c70a6068775ea927659898e09ae82
LordTriscuit97 <kirouacarnaud97@gmail.com>
2025-10-02 14:46:24 -0400

    code pomal complet

--- src/sifflet.cpp
+++ src/sifflet.cpp
@@ -1,1 +1,34 @@
 #include <LibRobus.h>
+
+const int pin5KHZ = 10;
+const int pinAmbiant = 11;
+
+const int seuilAsbolu = 1;
+const int seuilRelatif = 1;
+
+void setup() {
+  BoardInit();
+  pinMode(pin5KHZ, INPUT);
+  pinMode(pinAmbiant, INPUT);
+}
+
+float lecture5KHZ() {
+  return (analogRead(pin5KHZ)/1023)*5;
+}
+
+float lectureAmbiant() {
+  return (analogRead(pinAmbiant)/1023)*5;
+}
+
+bool detecteSifflet() {
+  float valeur5KHZ = lecture5KHZ();
+  float valeurAmbiant = lectureAmbiant();
+
+  if (valeur5KHZ > seuilAsbolu && (valeur5KHZ - valeurAmbiant) > seuilRelatif) {
+        return true;
+  } 
+
+  else {
+        return false;
+  }
+}
