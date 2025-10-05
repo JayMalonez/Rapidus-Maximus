292da647e36229ea4b8def3ca260e433263c07ed
jaypomaloney <mathisl525@gmail.com>
2025-10-05 17:45:13 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -21,9 +21,9 @@ void initParcours() {
   }
 
   //bordure du haut
-  parcours[0][0][0] = 1;
+  parcours[0][0][0] = 0;
   parcours[0][1][0] = 0; //Arrivée
-  parcours[0][2][0] = 1;
+  parcours[0][2][0] = 0;
 
   //bordure droite
   for (int i = 0; i < 10; i++) {
@@ -31,9 +31,9 @@ void initParcours() {
   }
   
   //bordure du bas
-  parcours[9][0][3] = 1;
-  parcours[9][1][3] = 1; //Départ
-  parcours[9][2][3] = 1;
+  parcours[9][0][2] = 1;
+  parcours[9][1][2] = 1; //Départ
+  parcours[9][2][2] = 1;
 
   //bordure gauche
   for (int i = 0; i < 10; i++) {
@@ -54,7 +54,8 @@ void initParcours() {
 }
 
 #define FULL_TURN_PULSE 3200
-#define QUARTER_ROTATION_PULSE 2133 //3200 * 2 / 3
+#define QUARTER_ROTATION_PULSE 2000 //2133 //3200 * 2 / 3
+
 
 #define DEBUG false
 
@@ -65,11 +66,20 @@ bool vert = false;
 bool rouge = false;
 int etat = 0; // = 0 arrêt 1 = avance 2 = recule 3 = TourneDroit 4 = TourneGauche
 int etatPast = 0;
-float vitesse = 0.2; //0,30
+float vitesse = 0.3; //0,30
 
 int pulseNbMain = 0;
 int pulseNbSub = 0;
 float ampliSub = 1;
+float slowAccelRight = 0;
+float slowAccelLeft = 0;
+
+const int pin5KHZ = 10;
+const int pinAmbiant = 11;
+
+const int seuilAsbolu = 1;
+const int seuilRelatif = 1;
+bool firstRun = true;
 
 void beep(int count){
   for(int i=0;i<count;i++){
@@ -86,20 +96,38 @@ void arret(){
   MOTOR_SetSpeed(LEFT, 0);
 };
 
-void avance(){
-  Serial.println("Avance");
-  MOTOR_SetSpeed(RIGHT, constrain(vitesse, 0.1, 0.5));
-  MOTOR_SetSpeed(LEFT, constrain(ampliSub*vitesse, 0.1, 0.5));
+void avance(int nbPulse){
+  ENCODER_ReadReset(RIGHT);
+  ENCODER_ReadReset(LEFT);
+  do {
+    slowAccelRight = ((float)(nbPulse/2 - abs(ENCODER_Read(RIGHT) - nbPulse/2)) / (float)(nbPulse)) + 0.1;
+    slowAccelLeft =  ((float)(nbPulse/2 - abs(ENCODER_Read(LEFT)  - nbPulse/2)) / (float)(nbPulse)) + 0.1;
+    MOTOR_SetSpeed(RIGHT, slowAccelRight);
+    MOTOR_SetSpeed(LEFT,  slowAccelLeft );
+    //Serial.println(slowAccelRight);
+  }
+  while(abs(ENCODER_Read(RIGHT)) < nbPulse);
 };
 
-void avance50(){
+/*void avance50(){
   ENCODER_Reset(RIGHT);
   ENCODER_Reset(LEFT);
-  while (ENCODER_Read(RIGHT) < 3000){
+;
+
+  while (true){
+    long droite = ENCODER_Read(RIGHT);
+    long gauche = ENCODER_Read(LEFT);
+    long moyenne = (droite + gauche) / 2;
+
+    if (moyenne >= 3800)
+    {
+      break;
+    }
+
     avance();
   }
   delay(1000);
-};
+};*/
 
 void tourne(char dir){
   bool leftMotorDone = false;
@@ -160,22 +188,60 @@ void calibrate(int timeToTestInSec){
   delay(100);
 }
 
+float lecture5KHZ() {
+  return (analogRead(pin5KHZ)/1023)*5;
+}
+
+float lectureAmbiant() {
+  return (analogRead(pinAmbiant)/1023)*5;
+}
+
+bool detecteSifflet() {
+  float valeur5KHZ = lecture5KHZ();
+  float valeurAmbiant = lectureAmbiant();
+
+  if (valeur5KHZ > seuilAsbolu && (valeur5KHZ - valeurAmbiant) > seuilRelatif) {
+        return true;
+  } 
+
+  else {
+        return false;
+  }
+}
+
 void setup() {
   BoardInit();
   initParcours();
   beep(3);
 
-  calibrate(5);
+  pinMode(pin5KHZ, INPUT);
+  pinMode(pinAmbiant, INPUT);
+  firstRun = true;
+
+  //calibrate(5);
   delay(1000);
   beep(2);
+  delay(5000);
 
   //case de départ
   currentTile[0] = 9;
   currentTile[1] = 1;
+
+  
 }
 
 void loop() {
-  if (currentTile[0] == 8) {
+  
+  if (firstRun){
+    
+    while(detecteSifflet() == false){
+      
+    }
+
+    firstRun = false;
+  }
+
+  if (currentTile[0] == -1) {
     Serial.println("Arrêt de l'algorithme");
     Serial.print(currentTile[0]);
     Serial.print(", ");
@@ -185,8 +251,8 @@ void loop() {
   }
 
   //regarde si on peut Avancer
-  if (parcours[currentTile[0]][currentTile[1]][0] == 0 /*&& digitalRead(vertpin) == 1 && digitalRead(rougepin) == 1*/) {
-    avance50();
+  if (parcours[currentTile[0]][currentTile[1]][0] == 0 && (digitalRead(vertpin) == 1 || digitalRead(rougepin) == 1)) {
+    avance(6250);
     currentTile[0]--;
 
     Serial.print(currentTile[0]);
@@ -196,33 +262,57 @@ void loop() {
   } else if (parcours[currentTile[0]][currentTile[1]][1] == 0)
   {
     tourne('R');
-    /*if (digitalRead(vertpin) == 0 && digitalRead(rougepin) == 0)
+    delay(1000);
+    arret();
+
+    if (digitalRead(vertpin) == 0 || digitalRead(rougepin) == 0)
     {
-     tourneGauche90(); //90
-     tourneGauche90(); //90
-     avance50();
-     tourneDroit90(); //se replace dans la bonne direction
-     currentTile[1]--;
+      tourne('L');
+      delay(400);
+      tourne('L');
+      delay(400);
+      avance(6250);
+      delay(400);
+      tourne('R'); //se replace dans la bonne direction
+      delay(1000);
     } else {
-      avance50();
-      currentTile[1]++;
+      avance(6250);
+      delay(400);
+      tourne('L');
+      delay(1000);
+
+      if (currentTile[1] == 0 && (digitalRead(vertpin) == 0 || digitalRead(rougepin) == 0))
+      {
+        Serial.print("continue");
+        tourne('L');
+        delay(400);
+        avance(6250);
+        delay(400);
+        avance(6250);
+        tourne('R');
+        delay(400);
+        currentTile[1]--;
+      } else
+      {
+        tourne('L');
+        currentTile[1]++;
+      }
 
       Serial.print(currentTile[0]);
       Serial.print(", ");
       Serial.print(currentTile[1]);
-      tourneGauche();
-    }*/
+    }
   //sinon vérifie à gauche
   } else if (parcours[currentTile[0]][currentTile[1]][3] == 0)
   {
     tourne('L');
-    //avance50();
+    avance(6250);
     currentTile[1]--;
 
     Serial.print(currentTile[0]);
     Serial.print(", ");
     Serial.print(currentTile[1]);
-    //tourneDroit90();
+    tourne('R');
   }
   delay(400);
   arret();
